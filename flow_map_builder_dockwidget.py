6e9ca51c2edd885ac639429a0eeff30e1560649d
glebpinigin <pinigin1514@live.com>
2022-03-31 15:15:46 +0300

    Adding dialog added

--- flow_map_builder_dockwidget.py
+++ flow_map_builder_dockwidget.py
@@ -49,17 +49,38 @@ class FlowMapBuilderDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
         self.add_tree.clicked.connect(self.addTree)
+        self.contexts = []
 
 
     def editingFinished(self, text):
         self.path = text
 
     def addTree(self):
-        self.currentContext = SpiralTreeContext()
-        self.tab_widget.setEnabled(True)
-        
-        #self.contextHub.append(SpiralTreeContext())
-
+        dlg = AddDialogWidget(dock=self)
+        if dlg.exec():
+            self.tab_widget.setEnabled(True)
+            self.currentContext = SpiralTreeContext(namestring=dlg.namestring)
+            self.contexts.append(self.currentContext)
+            self.context_hub.addItem(str(self.currentContext))
+        else:
+            pass
+    
     def closeEvent(self, event):
         self.closingPlugin.emit()
         event.accept()
+
+FORM_CLASS2, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'flow_map_builder_dockwidget_base.ui'))
+
+class AddDialogWidget(QtWidgets.QDialog, FORM_CLASS2):
+
+    def __init__(self, parent=None, dock=None):
+        super(AddDialogWidget, self).__init__(parent)
+        self.dock = dock
+        self.setupUI(self)
+        self.name_area.textChanged[str].connect(self.setName)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+    
+    def setName(self, name):
+        self.namestring=name
--- fm_template_models.py
+++ fm_template_models.py
@@ -1,6 +1,7 @@
 class SpiralTreeContext:
 
-    def __init__(self, lyr=None, expr=None, vol_flds=None, alpha=25, proj=None):
+    def __init__(self, namestring="", lyr=None, expr=None, vol_flds=None, alpha=25, proj=None):
+        self.namestring = namestring
         self.lyr = lyr
         self.expr = expr
         self.vol_flds = vol_flds
@@ -9,9 +10,13 @@ class SpiralTreeContext:
         self.styling_inactive = True
         self.saving_inactive = True
     
-    def updateContext(self, data=None, expr=None, vol_flds=None, alpha=None, proj=None):
-        self.data = data if data is not None else self.data
+    def updateContext(self, namestring=None, lyr=None, expr=None, vol_flds=None, alpha=None, proj=None):
+        self.namestring = namestring if namestring is not None else self.namestring
+        self.lyr = lyr if lyr is not None else self.lyr
         self.expr = expr if expr is not None else self.expr
         self.vol_flds = vol_flds if vol_flds is not None else self.vol_flds
         self.alpha = alpha if alpha is not None else self.alpha
         self.proj = proj if proj is not None else self.proj
+
+    def __repr__(self):
+        return self.namestring
